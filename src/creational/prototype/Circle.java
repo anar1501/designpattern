ab5435c78bd8ec4832a6e8de997e723ff961faf6
anar1501 <anar.memmedov1501@gmail.com>
2024-05-19 02:17:38 +0400

    separate each of class

--- src/creational/prototype/Circle.java
+++ src/creational/prototype/Circle.java
@@ -17,23 +17,3 @@ public class Circle implements Shape {
         System.out.println("Drawing a circle with radius " + radius);
     }
 }
-
-class Rectangle implements Shape {
-    private int width;
-    private int height;
-
-    public Rectangle(int width, int height) {
-        this.width = width;
-        this.height = height;
-    }
-
-    @Override
-    public Shape clone() {
-        return new Rectangle(this.width, this.height);
-    }
-
-    @Override
-    public void draw() {
-        System.out.println("Drawing a rectangle with width " + width + " and height " + height);
-    }
-}

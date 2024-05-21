eccaa7726e03b6dfc30ade73bc4931e1f26707d5
anar1501 <anar.memmedov1501@gmail.com>
2024-05-22 02:35:11 +0400

    changing decorator dp

--- src/structural/decorator/Main.java
+++ src/structural/decorator/Main.java
@@ -3,11 +3,9 @@ package structural.decorator;
 
 public class Main {
     public static void main(String[] args) {
-        Shape redCircle = new RedShapeDecorator(new Circle());
-        Shape redRectangle = new RedShapeDecorator(new Rectangle());
-        Shape blueRectangle = new BlueShapeDecorator(new Rectangle());
-        redCircle.draw();
-        redRectangle.draw();
-        blueRectangle.draw();
+        Pizza basicPizza = new BasicPizza();
+        System.out.println(basicPizza.getDescription() + " - $" + basicPizza.getCost());
+        Pizza deluxePizza = new ExtraCheese(new Pepperoni(new BasicPizza()));
+        System.out.println(deluxePizza.getDescription() + " - $" + deluxePizza.getCost());
     }
 }

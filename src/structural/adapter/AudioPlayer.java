d77b3cab70f3f93773a096ca0b300f6e41432941
anar1501 <anar.memmedov1501@gmail.com>
2024-05-24 05:33:44 +0400

    correction useless line

--- src/structural/adapter/AudioPlayer.java
+++ src/structural/adapter/AudioPlayer.java
@@ -5,11 +5,9 @@ public class AudioPlayer implements MediaPlayer {
 
     @Override
     public void play(String audioType, String fileName) {
-        // Inbuilt support for playing mp3 music files
         if (audioType.equalsIgnoreCase("mp3")) {
             System.out.println("Playing mp3 file. Name: " + fileName);
         }
-        // MediaAdapter provides support to play other file formats
         else if (audioType.equalsIgnoreCase("vlc") || audioType.equalsIgnoreCase("mp4")) {
             mediaAdapter = new MediaAdapter(audioType);
             mediaAdapter.play(audioType, fileName);
